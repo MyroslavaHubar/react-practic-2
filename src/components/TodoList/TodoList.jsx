8c489977fefe7ddfc86d3f008ef35b96655df578
yuliia_sharapova <juliaa.sharapovaa@gmail.com>
2024-08-03 18:59:25 +0300

    task3

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,12 +2,12 @@ import { Grid } from '..';
 import { TodoListItem } from '..';
 import { GridItem } from '..';
 
-export const TodoList = ({ array }) => {
+export const TodoList = ({ array, onDelete}) => {
   return (
     <Grid>
       {array.map((element, index) => (
         <GridItem key={element.id}>
-          <TodoListItem todo={element} index={index} />
+          <TodoListItem todo={element} index={index} onDelete={onDelete} />
         </GridItem>
       ))}
     </Grid>
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -3,14 +3,15 @@ import style from './TodoListItem.module.css';
 
 import { RiDeleteBinLine } from 'react-icons/ri';
 
-export const TodoListItem = ({ todo: { id, text }, index }) => {
+export const TodoListItem = ({ todo: { id, text }, index, onDelete }) => {
+  const handleClick = () => onDelete(id)
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
         TODO {index + 1}
       </Text>
       <Text>{text}</Text>
-      <button className={style.deleteButton} type="button">
+      <button className={style.deleteButton} type="button" onClick={handleClick}>
         <RiDeleteBinLine size={24} />
       </button>
     </div>
